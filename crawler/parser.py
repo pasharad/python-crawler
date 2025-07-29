91111a4fa373b6de271c9df1a441b2b4b039727c
amirpasha.pedramirad <amirpasha.pedramirad@snapp.cab>
2025-07-30 01:55:53 +0330

    design config map and modify parser more useful.

--- crawler/parser.py
+++ crawler/parser.py
@@ -1,21 +1,26 @@
 from bs4 import BeautifulSoup
 from typing import List, Dict
 
-def exctract_articles(soup: BeautifulSoup) -> List[Dict]:
+def exctract_articles(soup: BeautifulSoup, web: dict) -> List[Dict]:
     """
     Extracts all news articles from the soup.
-    Returns a list of dictionaries with title, description, date, url.
+    Returns a list of dictionaries with title, date, url.
     """
     articles = []
-    divs = soup.find_all("div", class_="listingResult")
-
+    divs = soup.find_all(web["divs"]["name"], class_=web["divs"]["class"])
     for div in divs:
         title_tag = div.find("h3")
-        date_tag = div.find("time")
+        if web["type"] == 1:
+            date_tag = div.find("span", class_="entry-meta-date updated").find("a").get_text(strip=True)
+            date = date_tag if date_tag else None
+        elif web["type"] == 2:
+            date_tag = div.find("time")
+            date = date_tag["datetime"] if date_tag and "datetime" in date_tag.attrs else None
         link_tag = div.find("a", href=True)
 
+
         title = title_tag.get_text(strip=True) if title_tag else None
-        date = date_tag["datetime"] if date_tag and "datetime" in date_tag.attrs else None
+        
         url = link_tag["href"] if link_tag else None
 
         articles.append({
@@ -26,14 +31,14 @@ def exctract_articles(soup: BeautifulSoup) -> List[Dict]:
 
     return articles
 
-def exctract_full_description(soup: BeautifulSoup) -> str:
+def exctract_full_description(soup: BeautifulSoup, id: str) -> str:
     """
     Fetches the article page and extracts full article content.
     Usually selects all <p> tags inside article body.
     """
     if not soup:
         return None
-    article_div = soup.find("div", id="article-body")
+    article_div = soup.find("div", id=id)
     if not article_div:
         return None
     paragraphs = article_div.find_all("p")
--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 from crawler.fetcher import get_soup
 from crawler.parser import exctract_articles, exctract_full_description
 
-url = "https://www.space.com/news"
-soup = get_soup(url)
 
-if soup:
-    articles = exctract_articles(soup)
-    for article in articles:
-        content = exctract_full_description(get_soup(article['url']))
-        print(f"🔭 {article['title']}")
-        print(f"📅 {article['date']}")
-        print(f"🔗 {article['url']}")
-        print(content)
-        print("------")
+webs = {
+    "spaceflightnow" : {
+        "type" : 1,
+        "link" : "https://spaceflightnow.com/category/news-archive/",
+        "pages" : 696,
+        "divs" : {"name":"header", "class":"mh-posts-list-header"},
+        "desc-tag" : "main-content"
+        },
+    "space" : {
+        "type" : 2,
+        "link" : "https://www.space.com/news",
+        "pages" : 9,
+        "divs" : {"name":"div", "class":"listingResult"},
+        "desc-tag" : "article-body"
+        }
+        }
+
+# url = "https://spaceflightnow.com/category/news-archive/"
+for web in webs:    
+    soup = get_soup(webs[web]["link"])
+
+    if soup:
+        articles = exctract_articles(soup, webs[web])
+        for article in articles:
+            content = exctract_full_description(get_soup(article['url']), webs[web]["desc-tag"])
+            print(f"🔭 {article['title']}")
+            print(f"📅 {article['date']}")
+            print(f"🔗 {article['url']}")
+            print(content)
+            print("------")
